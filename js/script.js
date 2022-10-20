112c781c84cf36da27440cf954c3dbc29378fd2d
Student <student@epicodus-5.hsd1.or.comcast.net>
2022-10-20 15:39:25 -0700

    add triangle math logic
    Co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
 window.addEventListener("load", function() {
+  // console.log("submittop")
   const form = document.getElementById("triangle");
-  form.addEventListener("submit", finalComp);
+  form.addEventListener("submit", triangleMath);
+  // console.log("submitbottom");
+  // console.log(num1);
 });
 
 //-------------------------------------------------------------
 function triangleMath(event) {
   event.preventDefault();
-  const num1 = parseInt(document.querySelector("input1"));
-  const num2 = parseInt(document.querySelector("input2"));
-  const num3 = parseInt(document.querySelector("input3"));
 
-  let result;
+  const num1 = parseInt(document.querySelector("input#input1").value);
+  const num2 = parseInt(document.querySelector("input#input2").value);
+  const num3 = parseInt(document.querySelector("input#input3").value);
+  console.log("TriangleMath");
+  console.log(num1);
   if (num1 === num2 && num2 === num3) {
-    "output".innerText = "Equallateral"
+    document.getElementById("output").innerText = "Equallateral";
   }
-  else if (num1 != num2 && num1 != num3 && num2 != num3){
-    "output".innerText = "Scallion Triangle"
+  else if (num1 + num2 <= num3 || num1 + num3 <= num2 || num2 + num3 <= num1){
+    document.getElementById("output").innerText = "Not a Triangle, Tri again";
   }
   else if (num1 === num2 || num1 === num3 || num2 === num3)
-  "output".innerText = "Flying-sicilies Triangle"
+  document.getElementById("output").innerText = "Flying-sicilies Triangle";
   else {
-    "output".innerText = "Not a triangle, Tri again."
+    document.getElementById("output").innerText = "Its a Scalene triangle!";
   }
 }
